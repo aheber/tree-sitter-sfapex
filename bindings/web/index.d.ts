264ac865c95f2da42504c2527fb5f53ee5f94d20
Heber <Anthony.Heber@gmail.com>
2025-02-11 11:32:01 -0700

    2.4.1 web bindings to re-export types

--- bindings/web/index.d.ts
+++ bindings/web/index.d.ts
@@ -1,4 +1,23 @@
-type Parser = import("web-tree-sitter");
+import TsParser from "web-tree-sitter";
+export type Parser = TsParser;
+export type {
+  Options,
+  Point,
+  Range,
+  Edit,
+  Logger,
+  Input,
+  SyntaxNode,
+  TreeCursor,
+  Tree,
+  QueryCapture,
+  QueryMatch,
+  QueryOptions,
+  PredicateResult,
+  Query,
+  LookaheadIterable,
+} from "web-tree-sitter";
+
 declare module "web-tree-sitter-sfapex" {
   function getApexParser(): Promise<Parser>;
   function getSoqlParser(): Promise<Parser>;
